d60784d9db6b5552933832c8a29fdc4c4adac965
odayushin <oda.yushin@gmail.com>
2021-07-16 20:46:04 +0900

    ファイルわけ

--- handler.js
+++ handler.js
@@ -1,32 +1,8 @@
 "use strict";
 const AWS = require("aws-sdk");
-const fs = require("fs");
-const s3Client = new AWS.S3({
-  s3ForcePathStyle: true,
-  accessKeyId: "S3RVER", // This specific key is required when working offline
-  secretAccessKey: "S3RVER",
-  endpoint: new AWS.Endpoint("http://localhost:4569"),
-});
 const dynamodbClient = new AWS.DynamoDB({
   endpoint: "http://localhost:8000",
 });
-
-module.exports.webhook = async (event, context, callback) => {
-  const uploadFile = fs.readFileSync("./upload_data.json");
-  const params = {
-    Bucket: "local-bucket",
-    Key: "upload_data.json",
-    Body: uploadFile,
-  };
-  const response = await s3Client
-    .putObject(params)
-    .promise()
-    .catch((err) => {
-      console.log(err, err.stack);
-    });
-  console.log("OK");
-  callback();
-};
 
 module.exports.hello = async (event) => {
   const params = {
